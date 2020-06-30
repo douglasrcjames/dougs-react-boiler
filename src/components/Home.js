2ddaf777d77b53aa647c9dfd2632b113253429c8
Douglas James <douglasrcjames@gmail.com>
2020-06-30 11:19:39 -0700

    Added react-flexbox-grid

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Grid, Row, Col } from 'react-flexbox-grid';
 
 export default class Home extends Component {
     render() {
@@ -7,6 +8,23 @@ export default class Home extends Component {
                 <h1>Home</h1>
                 {/* TODO: update content */}
                 <p>Welcome to Doug's React Boiler! Click around a bit, it's a clean slate for you!</p>
+                <br/>
+                <Grid fluid>
+                    <Row center="xs">
+                        <Col xs={12} sm={4}>
+                            <h3>Column 1</h3>
+                            <p>More information below</p>
+                        </Col>
+                        <Col xs={12} sm={4}>
+                            <h3>Column 2</h3>
+                            <p>More information below</p>
+                        </Col>
+                        <Col xs={12} sm={4}>
+                            <h3>Column 3</h3>
+                            <p>More information below</p>
+                        </Col>
+                    </Row>
+                </Grid>
             </div>
         )
     }
